99dc4f16073808ad3e490ca6271f14f17ddc19c1
Cett <florian_steinmann@hotmail.de>
2025-02-25 10:17:21 +0100

    storage changes

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,12 @@
 import { defineStorage } from '@aws-amplify/backend';
 
+
 export const storage = defineStorage({
-    name: 'fileHosterStorage'
+    name: 'fileHosterStorage',
+    access: (allow) => ({
+        'files/*': [
+            allow.authenticated.to(['read','write']),
+            allow.guest.to(['read'])
+        ],
+    })
 });
